2b968f08bce127a79487d7f70846b537cb052568
WarojY <warojy62@nu.ac.th>
2021-07-16 13:47:18 +0700

    [U4L] Databases Labs

---
+++ u4-databases/features/uni-controller.js
@@ -0,0 +1,54 @@
+const { db } = require('../_services/firebase-initialized')
+
+const iterationLists = async (req, res) => {
+  const query = db.collection('iterations').get()
+  const iteration = (await query).docs.map(doc => doc.data())
+
+  res.render('uni-iteration-list', { iteration })
+}
+
+const iterationDetails = async (req, res) => {
+  const code = req.params.code
+  const query = db.collection('iterations').doc(code).get()
+  const iteration = (await query).data()
+
+  res.render('uni-iteration-details', { iteration })
+}
+
+const studentDetails = async (req, res) => {
+  const code = req.params.code
+  const query = db.collection('students').doc(code).get()
+  const student = (await query).data()
+
+  res.render('uni-student-details', { student })
+}
+
+const courseLists = async (req, res) => {
+  const query = db.collection('iterations')
+    .where('year', '==', 2021)
+    .where('semester', '==', 1)
+    .get()
+  const iteration = (await query).docs.map(doc => doc.data())
+
+  res.render('uni-course-list', { iteration })
+}
+
+const courseDetails = async (req, res) => {
+  const code = req.params.code
+  const query = db.collection('iterations')
+    .where('courseCode', '==', code)
+    .orderBy('iterationCode', 'desc')
+    .limit(1)
+    .get()
+  const iteration = (await query).docs[0].data()
+
+  res.render('uni-course-details', { iteration })
+}
+
+module.exports = {
+  iterationLists,
+  iterationDetails,
+  studentDetails,
+  courseLists,
+  courseDetails
+}

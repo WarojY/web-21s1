54571a230db95a756fad3a6465d265cc99da8982
WarojY <warojy62@nu.ac.th>
2021-08-19 09:34:03 +0700

    [MP1] Index Controller

--- proj1-serverside-cinema/app.js
+++ proj1-serverside-cinema/app.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const { json, urlencoded } = require('body-parser')
 
+const { index } = require('./features/index-controller')
+
 const app = express()
+
+// Templates
+app.set('views', './views')
+app.set('view engine', 'hbs')
+app.engine('hbs', exphbs({
+  extname: '.hbs',
+  defaultLayout: false,
+  partialsDir: ['./views/partials', './views/layouts']
+}))
 
 // Middleware
 app.use(express.static('public'))
@@ -9,5 +21,6 @@ app.use(json())
 app.use(urlencoded({ extended: false }))
 
 // Routes
+app.get('/', index)
 
 module.exports = { app }
